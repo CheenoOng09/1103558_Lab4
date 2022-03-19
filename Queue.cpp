d2f95ee6e158bb6ba2c0474dbba9cd07a221b5c5
Nakdimon <gjherrera.i@gmail.com>
2022-03-19 16:19:42 +0800

    Organized includes and formatting

--- Queue.cpp
+++ Queue.cpp
@@ -10,25 +10,29 @@ int Front = 0;
 int Rear = 0;
 int Count = 0;
 
-Queue::Queue(int size){
+Queue::Queue(int size)
+{
     Size = size;
     list<int> queueList;
-    intQueue = & queueList;    
+    intQueue = &queueList;
     cout << "-- A Queue of " << Size << " is created." << endl;
 
-}// constructor with size initialized to 10
+} // constructor with size initialized to 10
 
-Queue::~Queue(){
-    cout << "-- A Queue of size " << Size << " is destroyed." << endl; 
+Queue::~Queue()
+{
+    cout << "-- A Queue of size " << Size << " is destroyed." << endl;
 
 } // destructor
 
-void Queue::enqueue(int element){
+void Queue::enqueue(int element)
+{
     intQueue->push_back(element);
-    // Rear = intQueue->size()-1; 
+    // Rear = intQueue->size()-1;
 } // Insert an element in the rear end of a Queue
 
-int Queue::dequeue(){
+int Queue::dequeue()
+{
     int frontValue = intQueue->front();
     intQueue->pop_front();
     // Front = intQueue->front();
@@ -36,21 +40,25 @@ int Queue::dequeue(){
 } // remove and then return the front most element in the Queue OR rescheduling waiting list based on the order
 
 // when the process gets rescheduled + 2 for the variable execution time
-int Queue::peek() {
+int Queue::peek()
+{
     return Front;
 } // return the front most element in the queue
 
-int Queue::getCount(){
+int Queue::getCount()
+{
     return Count;
-}// return the number of elements stored in the Queue
+} // return the number of elements stored in the Queue
 
 int Queue::getSize()
 {
     return Size;
-}// return Queue Size
+} // return Queue Size
 
-bool Queue::isEmpty(){
-    if (getCount() == 0){
+bool Queue::isEmpty()
+{
+    if (getCount() == 0)
+    {
         cout << "-- Warning: Queue is EMPTY now." << endl;
         return true;
     }
@@ -59,8 +67,10 @@ bool Queue::isEmpty(){
     // if empty, that means the program has finished processing all the waiting list and we should the time execution
 }
 
-bool Queue::isFull(){
-    if(getCount() == getSize()){
+bool Queue::isFull()
+{
+    if (getCount() == getSize())
+    {
         cout << "-- Warning: Queue is FULL now." << endl;
         return true;
     }
@@ -68,11 +78,12 @@ bool Queue::isFull(){
         return false;
 }
 
-void Queue::clearQueue(){
+void Queue::clearQueue()
+{
     intQueue = NULL;
     Count = 0;
-    Front = NULL;
-    Rear = NULL;
+    Front = 0;
+    Rear = 0;
 } // clear the content of queue and reset front, rear, and count
 
 // void printList(Node* n)
@@ -83,10 +94,10 @@ void Queue::clearQueue(){
 //     }
 // }
 
-void Queue::printQueue() {
-    cout<< "List printed" <<endl;
+void Queue::printQueue()
+{
+    cout << "List printed" << endl;
     // for (auto const &i: list) {
     //     std::cout << i << std::endl;
     // }
 } // print the content of a queue from front to rear
-
